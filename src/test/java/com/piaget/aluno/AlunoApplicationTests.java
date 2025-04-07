6a1941bcdc52437b34fd03dbca261472abe80204
luizbenicio2004 <luizbenicio007@gmail.com>
2025-04-07 20:52:34 -0300

    first commit

---
+++ src/test/java/com/piaget/aluno/AlunoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piaget.aluno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlunoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
